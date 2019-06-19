b41ac0b14db7fcd1bd1eb53d88383ff4bb5dc622
zhangkaihuo <zhangkaihuo@cortexlabs.ai>
2019-06-19 20:54:05 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -17,6 +17,5 @@ worker='zkh'
 pool_uri='cuckoo.cortexlabs.ai:8008'
 device=$str
 account='0x2dadd26ed620609751eb439edfb2490fce08829a'
-start='./build/bin/cortex_miner -pool_uri=i'$pool_uri' -worker='$worker' -devices='$str' account='$account #0x2dadd26ed620609751eb439edfb2490fce08829a'
-echo $start
-$start
+start='./build/bin/cortex_miner -pool_uri='$pool_uri' -worker='$worker' -devices='$str' account='$account #0x2dadd26ed620609751eb439edfb2490fce08829a'
+echo $start$start
