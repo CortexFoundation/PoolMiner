c63885624f3416d0a4995ffa14f5dbff12c580bc
zhangkaihuo <zhangkaihuo@cortexlabs.ai>
2019-06-19 19:28:18 +0800

    start and stop bash

---
+++ start.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64/:/usr/local/cuda/lib64/stubs:$LD_LIBRARY_PATH
+export LIBRARY_PATH=/usr/local/cuda/lib64/:/usr/local/cuda/lib64/stubs:$LIBRARY_PATH
+export PATH=/usr/local/go/bin:/usr/local/cuda/bin/:$PATH
+NVIDIA_COUNT=`nvidia-smi -L | grep 'GeForce' | wc -l`
+echo "there has "$NVIDIA_COUNT "gpu"
+str=""
+for ((i=0; i < $NVIDIA_COUNT; i++))
+do
+ str=$str$i
+ if [ $i -lt $[$NVIDIA_COUNT - 1] ]; then
+  str=$str,
+ fi
+done
+start='./build/bin/cortex_miner -pool_uri=cuckoo1.cortexlabs.ai:8008 -devices='$str' account=0x2dadd26ed620609751eb439edfb2490fce08829a'
+$start
